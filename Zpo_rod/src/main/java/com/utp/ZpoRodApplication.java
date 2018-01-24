1b4498868502adbe51a1902f837c32e8a593e4c1
Adam <przedlacki.adam@gmail.com>
2018-01-24 14:20:39 +0100

    first commit

---
+++ Zpo_rod/src/main/java/com/utp/ZpoRodApplication.java
@@ -0,0 +1,12 @@
+package com.utp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZpoRodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZpoRodApplication.class, args);
+	}
+}
